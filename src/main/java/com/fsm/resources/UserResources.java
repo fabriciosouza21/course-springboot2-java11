3eaba6c782dc25d51ce57cb7c31a4dfb6ea2a75c
fabriciosouza21 <fabriciosouza21777@gmail.com>
2021-07-07 15:14:52 -0300

    User delete

--- src/main/java/com/fsm/resources/UserResources.java
+++ src/main/java/com/fsm/resources/UserResources.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,28 +20,35 @@ import com.fsm.services.UserServices;
 @RestController
 @RequestMapping(value = "/users")
 public class UserResources {
-	
+
 	@Autowired
 	UserServices services;
-	
+
 	@GetMapping
-	public ResponseEntity<List<User>> findAll(){
-		
+	public ResponseEntity<List<User>> findAll() {
+
 		List<User> list;
 		list = services.findAll();
 		return ResponseEntity.ok().body(list);
 	}
+
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<User> findById(@PathVariable Long id){
+	public ResponseEntity<User> findById(@PathVariable Long id) {
 		User obj = services.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
+
 	@PostMapping
-	public ResponseEntity<User> insert(@RequestBody User obj){
+	public ResponseEntity<User> insert(@RequestBody User obj) {
 		obj = services.insert(obj);
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
-				.buildAndExpand(obj.getId()).toUri();
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).body(obj);
-		
+
 	}
+
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		services.delete(id);
+		return ResponseEntity.noContent().build();	
+		}
 }
